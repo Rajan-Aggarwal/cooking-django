59f82988bd534908b73013b780e1deea49591c03
Rajan-Aggarwal <rajansien@gmail.com>
2019-07-14 09:52:38 +0100

    Add httpd disable

--- recipes/nginx.rb
+++ recipes/nginx.rb
@@ -13,6 +13,14 @@ template '/etc/nginx/nginx.conf' do
   notifies :restart, 'service[nginx]', :immediately
 end
 
+# if apache httpd service is enabled
+# disable it
+if File.exists?('/usr/lib/systemd/system/httpd.service')
+  service 'httpd' do
+    action [:stop, :disable]
+  end
+end
+
 # enable and restart the service upon running the chef-client
 service 'nginx' do
   action [:restart, :enable]
